2c38634cd2f5f69370017a382baf0f32885a6510
nihad1213 <nihad.nemetli@gmail.com>
2025-02-27 20:10:31 +0400

    theme added

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -1,10 +1,15 @@
 <?php
-while (have_posts()) {
-    the_post();
-    ?>
+
+  get_header();
+
+  while(have_posts()) {
+    the_post(); ?>
     <h1>This is a page not a post</h1>
-    <h2><?php the_title() ?></h2>
-    <?php the_content() ?>
-    <?php
-}
+    <h2><?php the_title(); ?></h2>
+    <?php the_content(); ?>
+    
+  <?php }
+
+  get_footer();
+
 ?>
