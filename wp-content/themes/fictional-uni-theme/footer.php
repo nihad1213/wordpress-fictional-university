fcefa760a0347b6c750f7c6c73b13647eac23626
nihad1213 <nihad.nemetli@gmail.com>
2025-05-28 12:28:01 +0400

    small fix

--- wp-content/themes/fictional-uni-theme/footer.php
+++ wp-content/themes/fictional-uni-theme/footer.php
@@ -21,7 +21,7 @@
             </div>
 
             <div class="site-footer__col-three">
-              <h3 class="headline headline--small">Learn</h3>
+              <h3 class="headline headline--small">Learn</h3> 
               <nav class="nav-list">
               <?php 
                   wp_nav_menu([
