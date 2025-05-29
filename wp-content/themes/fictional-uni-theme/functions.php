2294bba69494806bb0fcfc3bd3c9af429f5e0b55
nihad1213 <nihad.nemetli@gmail.com>
2025-05-29 10:13:41 +0400

    update in functions.php file

--- wp-content/themes/fictional-uni-theme/functions.php
+++ wp-content/themes/fictional-uni-theme/functions.php
@@ -19,3 +19,17 @@ function university_features() {
 }
 
 add_action('after_setup_theme', 'university_features');
+
+
+function university_post_types() {
+  register_post_type('event', [
+    'public' => true,
+    'labels' => [
+      'name' => 'Events',
+    ],
+    'menu_icon' => 'dashicons-calendar-alt'
+
+  ]);
+}
+
+add_action('init', 'university_post_types');
